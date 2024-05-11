be672ebbda1f8a96be31f9d4cc0b13b4c515e290
JaViKo500 <javikogutierrez64@gmail.com>
2024-05-11 15:48:48 -0500

    add image gift in discord

--- src/presentation/services/discord.service.ts
+++ src/presentation/services/discord.service.ts
@@ -9,6 +9,13 @@ export class DiscordService {
    async notify( message: string ) {
       const body = {
          content: message,
+         embeds: [
+            {
+               image: {
+                  url: 'https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExMmt1MG1jc3RlbHpsbWM0aTB0eW5sancyNWt0dmhwY243ZTY2dDd1YiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/du3J3cXyzhj75IOgvA/giphy.gif',
+               },
+            }
+         ]
       };
 
       const response = await fetch(
